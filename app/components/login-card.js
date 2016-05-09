9edee560add1f21708dc6233942b83fd82c67a40
Marcelo J. Both <marcelo.both@gmail.com>
2016-05-08 21:58:07 -0300

    Login register (#2)
    * Login and register pages
    
    * Client-side validations
    
    * Fixed merge issues
    
    * Fixed merge issues
    
    * User registration UI

--- app/components/login-card.js
+++ app/components/login-card.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({
+import { email, password } from '../utils/user-validations';
+import { buildValidations } from 'ember-cp-validations';
+
+const { Component } = Ember;
+
+const Validations = buildValidations({
+  'model.email': email,
+  'model.password': password
+});
+
+export default Component.extend(Validations, {
+
 });
